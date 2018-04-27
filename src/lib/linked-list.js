aeb6f18bb0c7fbd4e5e787681402b84e98a25465
Josiah Green <JosiahGreen90@gmail.com>
2018-04-27 15:31:53 -0700

    scaffolded

---
+++ src/lib/linked-list.js
@@ -0,0 +1,102 @@
+'use strict'; 
+
+const Node = require('./node');
+const logger = require('./logger');
+
+module.exports = class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertAtHead(value) {
+    const node = new Node(value);
+
+    node.next = this.head;
+    this.head = node; 
+    return this;
+  }
+
+  insertAtEnd(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+
+  find(value) {
+    if (!this.head) {
+      logger.log(logger.INFO, '#find Linked List array is empty. Could not find value.');
+      return null;
+    }
+
+    if (this.head.value === value) {
+      return this.head;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        return currentNode.next;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  map(callback) {
+    if (!this.head) {
+      logger.log(logger.INFO, '#method Array is empty. Could not find value.');
+      return null;
+    }
+    let currentNode = this.head;
+
+    while (currentNode) {
+      currentNode.value = callback(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return this;
+  }
+
+  pop() {
+    let currentNode = this.head;
+    if (!this.head) {
+      return null;
+    }
+    if (!currentNode.next) {
+      currentNode = null;
+      return currentNode;
+    }
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+        return null;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+  
+  remove(value) {
+    if (!this.head) {
+      logger.log(logger.INFO, '#remove Linked List array is empty. Could not find value.');
+      return null;
+    }
+
+    if (this === value) {
+      delete this.value;
+    } else {
+      logger.log(logger.INFO, '#remove Value does not exist in list.');
+    }
+    return undefined;
+  }
+};
